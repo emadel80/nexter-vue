aaa7f3582f7bf22a26d2af0ae64ee5ea62408d17
Emanuel Del Castillo <emanuel.delcastillo@gmail.com>
2022-07-29 20:12:32 -0400

    Sidebar component

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,12 @@
+<script setup>
+import NavButton from "@/components/Button.vue"
+</script>
+
 <template>
 	<aside class="sidebar">
-		Sidebar
+		<NavButton class="nav-btn">
+			<div class="hamburger-icon"></div>
+		</NavButton>
 	</aside>
 </template>
 
@@ -10,5 +16,40 @@
 
 	grid-column: sidebar-start / sidebar-end;
 	grid-row: 1 / -1;
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
+.hamburger-icon {
+	border: none;
+	border-radius: 0;
+	margin-top: 2rem;
+	background-color: #fff;
+	height: 3px;
+	width: 3.5rem;
+
+	&::before,
+	&::after {
+		content: "";
+		background-color: #fff;
+		height: 3px;
+		width: 3.5rem;
+		display: block;
+		text-align: center;
+	}
+
+	&::before {
+		transform: translateY(-1.5rem);
+	}
+
+	&::after {
+		transform: translateY(1.3rem);
+	}
+}
+
+.nav-btn {
+	padding-bottom: 3.8rem;
 }
 </style>
